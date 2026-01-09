b1d982d0ef8f73762cddcef7983d694fd105e995
Sleepywolf <57480382+sleepywolf-exe@users.noreply.github.com>
2026-01-09 18:03:08 +0530

    update

--- check_dest_images.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/db.php';
-$db = Database::getInstance();
-$destinations = $db->fetchAll("SELECT id, name, image_url, slug FROM destinations");
-foreach ($destinations as $d) {
-    echo "ID: " . $d['id'] . " | Name: " . $d['name'] . " | Slug: " . $d['slug'] . " | Image: " . $d['image_url'] . "\n";
-}
-
-?>
