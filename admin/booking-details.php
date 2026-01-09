a42d87badded74e2bc1450de60a93b4cf5c61680
Sleepywolf <57480382+sleepywolf-exe@users.noreply.github.com>
2026-01-09 20:52:11 +0530

    Add static favicon to admin/booking-details.php

--- admin/booking-details.php
+++ admin/booking-details.php
@@ -53,6 +53,7 @@ if (!$booking) {
     <title>Booking #
         <?php echo $id; ?> - Admin
     </title>
+    <link rel="shortcut icon" href="<?php echo base_url('assets/images/admin-favicon.png'); ?>" type="image/x-icon">
     <script src="https://cdn.tailwindcss.com"></script>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;500;600&display=swap" rel="stylesheet">
     <style>
