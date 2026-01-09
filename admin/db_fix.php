394e209ecd8bde903805ea384f2e142a63cef76f
Sleepywolf <57480382+sleepywolf-exe@users.noreply.github.com>
2026-01-09 17:31:35 +0530

    Update db_fix.php

--- admin/db_fix.php
+++ admin/db_fix.php
@@ -18,6 +18,8 @@ $commands = [
     "ALTER TABLE inquiries ADD COLUMN utm_source VARCHAR(255)",
     "ALTER TABLE inquiries ADD COLUMN utm_medium VARCHAR(255)",
     "ALTER TABLE inquiries ADD COLUMN utm_campaign VARCHAR(255)",
+    "ALTER TABLE packages ADD COLUMN activities TEXT DEFAULT NULL",
+    "ALTER TABLE packages ADD COLUMN themes TEXT DEFAULT NULL",
     "ALTER TABLE packages ADD COLUMN trust_badges TEXT DEFAULT NULL"
 ];
 
