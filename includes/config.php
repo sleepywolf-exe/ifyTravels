8932daa6d66aab6ca8c7054f0515165728ad5741
Sleepywolf <57480382+sleepywolf-exe@users.noreply.github.com>
2026-01-31 18:25:25 +0530

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -2,9 +2,8 @@
 // includes/config.php
 
 // DB_CONNECTION: 'sqlite' or 'mysql'
-// DB_CONNECTION: 'sqlite' or 'mysql'
 // Default to MySQL for Cloud/cPanel deployment. Change to 'sqlite' if testing locally without MySQL.
-define('DB_CONNECTION', 'sqlite');
+define('DB_CONNECTION', 'mysql');
 
 // Database Credentails (cPanel / Remote)
 // Ignored when using SQLite
