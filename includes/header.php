27681aaed39004afcad0257e31b128268f64219f
Sleepywolf <57480382+sleepywolf-exe@users.noreply.github.com>
2026-02-06 15:13:10 +0530

    Perf: Switch font-display to block to prevent FOUC

--- includes/header.php
+++ includes/header.php
@@ -138,7 +138,7 @@ if (isset($_GET['ref']) && !empty($_GET['ref'])) {
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
-        href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,400&family=Great+Vibes&display=swap"
+        href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,400&family=Great+Vibes&display=block"
         rel="stylesheet">
 
     <!-- GSAP & Lenis (Smooth Scroll) -->
